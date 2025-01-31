c197eaf87119098f86423fd5dc28188d4fcd51d9
Mezhibo Evgeniy <mezhibo@example.com>
2025-01-31 20:41:37 +0300

    1

--- variables.tf
+++ variables.tf
@@ -1,1 +1,98 @@
-variable "yc_token" {}
+#cloud vars
+
+variable "cloud_id" {
+  type        = string
+  default     = "b1gvqb4s3f495f55ih0b"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1glq93bir0j2f0sl892"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "account_name" {
+  type        = string
+  default     = "service"
+  description = "account_name"
+}
+
+variable "kuber" {
+  type        = string
+  default     = "kuber"
+  description = "account_name"
+}
+
+# IP
+variable "host_ip" {
+  default = "0.0.0.0/0"
+}
+
+
+variable "vpc_name" {
+  type        = string
+  default     = "vpc0"
+  description = "VPC network"
+}
+
+variable "subnet-a" {
+  type        = string
+  default     = "subnet-a"
+  description = "subnet name"
+}
+
+variable "subnet-b" {
+  type        = string
+  default     = "subnet-b"
+  description = "subnet name"
+}
+
+variable "subnet-d" {
+  type        = string
+  default     = "subnet-d"
+  description = "subnet name"
+}
+
+variable "zone-a" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "zone-b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "zone-d" {
+  type        = string
+  default     = "ru-central1-d"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "cidr-a" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "cidr-b" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "cidr-d" {
+  type        = list(string)
+  default     = ["10.0.3.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
